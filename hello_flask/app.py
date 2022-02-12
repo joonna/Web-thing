dc9e4d61548cdd4d5464976c8cb7842b4b6f60c7
Jonathan <43124873+joonna@users.noreply.github.com>
2022-02-12 13:11:26 -0800

    Add files via upload

---
+++ hello_flask/app.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,request
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+
+import datetime
+
+app = Flask(__name__)
+FlaskJSON(app)
+
+USER_PASSWORDS = { "cjardin": " strong password"}
+
+IMGS_URL = {
+            "DEV" : "/static",
+            "INT" : "https://cis-444-fall-2021.s3.us-west-2.amazonaws.com/images",
+            "PRD" : "http://d2cbuxq67vowa3.cloudfront.net/images"
+            }
+
+CUR_ENV = "PRD"
+
+@app.route('/') #endpoint
+def index():
+    return 'Web App with Python Caprice!' + USER_PASSWORDS['cjardin']
+
+@app.route('/buy') #endpoint
+def buy():
+    return 'Buy'
+
+@app.route('/hello') #endpoint
+def hello():
+    return render_template('hello.html',img_url=IMGS_URL[CUR_ENV] ) 
+
+@app.route('/back',  methods=['GET']) #endpoint
+def back():
+    return render_template('backatu.html',input_from_browser=request.args.get('usay', default = "nothing", type = str) )
+
+@app.route('/backp',  methods=['POST']) #endpoint
+def backp():
+    return render_template('backatu.html',input_from_browser= str(request.form) )
+
+@app.route('/auth',  methods=['POST']) #endpoint
+def auth():
+        #print(request.form['username'])
+        return json_response(data=request.form)
+
+
+
+#Assigment 2
+@app.route('/ss1') #endpoint
+def ss1():
+    return render_template('server_time.html', content="The society that separates its scholars from its warriors will have its thinking done by cowards and its fighting by fools.")
+
+app.run(host='0.0.0.0', port=80)
+
